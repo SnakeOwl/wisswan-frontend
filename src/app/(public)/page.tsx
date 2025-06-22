772a5d351f9e23447727bb361f27f42e8cc81042
Nikolay <kolyania1@gmail.com>
2025-06-23 00:13:53 +0300

    обновил tailwind

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,4 +1,11 @@
 import clsx from 'clsx';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+    title: "WriteWrite"
+}
+
 
 export default async function Page() {
     return (
@@ -7,6 +14,10 @@ export default async function Page() {
             <div className={clsx("text-green-500", {
                 "text-red-400": true
             })}>
+
+            <div className="w-[50px] h-[50px] bg-red-500 hover:bg-yellow-500"></div>
+            <h1 className='crimson'>h1</h1>
+            <h2>h2</h2>
 
             </div>
 
